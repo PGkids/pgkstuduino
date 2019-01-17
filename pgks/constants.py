80682b025c93ae6bfa7dc3f775a99a3ef01eeb0f
PGkids Laboratory <46478949+PGkids@users.noreply.github.com>
2019-01-17 18:53:28 +0900

    dev

---
+++ pgks/constants.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2019 PGkids Laboratory
+
+from studuino import A0,A1,A2,A3,A4,A5,A6,A7,M1,M2,D2,D4,D7,D8,D9,D10,D11,D12
+
+conn_dic = {'A0':A0, 'A1':A1, 'A2':A2, 'A3':A3,
+            'A4':A4, 'A5':A5, 'A6':A0, 'A7':A0,
+            'M1':M1, 'M2':M2,
+            'D2':D2, 'D4':D4, 'D7':D7, 'D8':D8,
+            'D9':D9, 'D10':D10, 'D11':D11, 'D12':D12
+            }
+            
+
+# コネクタ
+def conn(ident):
+    ident = ident.upper()
+    if ident == 'A':
+        return (A0,A1,A2,A3,A4,A5,A6,A7)
+    elif ident == 'D':
+        return (D2,D4,D7,D8,D9,D10,D11,D12)
+    elif ident == 'M':
+        return (M1,M2)
+    elif ident in conn_dic:
+        return conn_dic[ident]
+    else:
+        raise(NameError)
+
+
+note_dic = {'C':0,
+            'C+':1, 'D-':1
+            'D':2,
+            'D+':3,'E-':3,
+            'E':4, 'F-':4,
+            'F':5, 'E+':5,
+            'F+':6, 'G-':6,
+            'G':7,
+            'G+':8, 'A-':8,
+            'A':9,
+            'A+':10,'B-':10,
+            'B':11,'C-':11}
+
+# note('C')==60, note('C',3)=48
+
+def note(ident, octave=4):
+    return 12 + 12*note_dic[ident]
+
