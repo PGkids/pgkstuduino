f320700feb6d7d1b24ef55f77a44e077d5054186
PGkids Laboratory <46478949+PGkids@users.noreply.github.com>
2019-01-19 18:11:42 +0900

    dev

--- pgks/wrapped_classes.py
+++ pgks/wrapped_classes.py
@@ -3,55 +3,77 @@
 
 from studuino import (LED,Buzzer,DCMotor,Servomotor,Accelerometer,PushSwitch,TouchSensor,
                       IRPhotoreflector,LightSensor,SoundSensor,
-                      FWD,BCK)
-from threading import Thread
+                      FWD,BCK,BRAKE,COAST)
+from threading import Thread,Event
 from time import sleep
 from math import ceil
 
-class RobotJob():
-    def __init__(self, proc, initializer=None, finalizer=None):
-        self._active = True
-        def f():
-            if initializer: initializer(self)
-            proc(self)
-            if finalizer: finalizer(self)
-        self._thread = Thread(target=f)
-        self._thread.start()
 
-    def _ssleep(self, rsec):
-        if not self._active:
-            return False
-        elif rsec>0.1:
-            m = ceil(rsec/0.1)
-            shredded_rsec = rsec/m
-            while self._active and m>0:
-                m -= 1
-                sleep(shredded_rsec)
-        else:
-            sleep(rsec)
-        return self._active
+class RobotJob():
+    __thread = None
+    __event = None
     
+    def __init__(self, proc):
+        #def f():
+        #    proc(self)
+        self.__thrproc = lambda:proc(self)
+
+    def __call__(self):
+        self.start()
+        self.wait()
+        
+    # def _ssleep(self, sec):
+    #     if not self._active:
+    #         return False
+    #     elif sec>0.1:
+    #         m = ceil(sec/0.1)
+    #         shredded_sec = sec/m
+    #         while self._active and m>0:
+    #             m -= 1
+    #             sleep(shredded_sec)
+    #     else:
+    #         sleep(sec)
+    #     return self._active
+
+    def _ssleep(self, sec):
+        if not self.__event:
+            return False
+        return self.__event.wait(sec)
+
+    def is_active(self):
+        return (self.__event and not self.__event.is_set())
+    
+    def start(self):
+        self.__event = Event()
+        self.__thread = Thread(target=self.__thrproc)
+        self.__thread.start()
+        return self
+
     def wait(self):
-        self._thread.join()
+        self.__thread.join()
+        self.__thread = None
     
     def cancel(self):
-        self._active = False
+        self.__event.set()
         self.wait()
 
-    def get_thread(self):
-        return self._thread
+def mkjob(proc) -> RobotJob:
+    return RobotJob(lambda job:proc())
+
+def parjobs(*jobs) -> RobotJob:
+    pass
 
 # 出力系パーツ
 
 class LED_wrap(LED):
     # n==None means n is infinity
-    def blink(self, n=None, on=0.3, off=0.3, initializer=None, finalizer=None):
+    def blink(self, n=None, on=0.3, off=0.3):
         def f(job):
-            nonlocal n
-            while job._active:
-                if n is not None:
-                    if n==0: break
-                    n -= 1
+            cnt = n
+            while job.is_active():
+                if cnt is not None:
+                    if cnt==0: break
+                    cnt -= 1
                 print('on')
                 if not job._ssleep(on):
                     # todo: off にしてから
@@ -59,9 +81,9 @@ class LED_wrap(LED):
                 print('off')
                 job._ssleep(off)
                 
-        return RobotJob(f,initializer,finalizer)
+        return RobotJob(f)
                                
-                        
+
 class Buzzer_wrap(Buzzer):
     def on(self, note, octave=0, sec=None, duration=None):
         if sec: d = int(sec*1000)
@@ -76,13 +98,18 @@ class Buzzer_wrap(Buzzer):
 
 class DCMotor_wrap(DCMotor):
     _direction = FWD
-    def setdirection(self):
-        pass
-    def setbreak(self):
-        pass
-    def drive(self,sec):
+    def drive(self,sec,forward=True,brake=True):
         pass
 
+    def moveon(self,forward=True):
+        self.move(FWD if forward else BCK)
+
+    # DCMotor.stop()を遮蔽することに注意（互換性あり）
+    def stop(motion=BRAKE, brake=True):
+        if motion==BRAKE and not brake: motion = COAST
+        DCMotor.stop(self, motion)
+    
+    
 ## 入力系パーツ デジタルセンサ
 
 class PushSwitch_wrap(PushSwitch):
