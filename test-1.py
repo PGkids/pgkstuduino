15783e1db3ac2579513a03b93e43b58f93980455
PGkids Laboratory <46478949+PGkids@users.noreply.github.com>
2019-01-22 12:44:04 +0900

    dev

--- test-1.py
+++ test-1.py
@@ -1,13 +1,17 @@
 # -*- coding: utf-8 -*-
 from pgkstuduino import *
 
+# 3つのLEDを異なる発光パターンで同時にブリンクさせる並列処理の例
+
 connect(4)
 
 led1,led2,led3 = mkpart('LED:A0/A1/A2')
-j1 = par(led1.job_blink(n=10,on=0.4,off=0.1),
-         led2.job_blink(n=10,on=0.2,off=0.3),
-         led3.job_blink(n=10,on=1,off=0.2))
-job = j1.start() #lim(j1, 5, False).start()
+job = par(led1.job_blink(n=10,on=0.4,off=0.1),
+          led2.job_blink(n=10,on=0.2,off=0.3),
+          led3.job_blink(n=5,on=1,off=0.2))
+job.start()
 job.join()
+
+# job.start().join()と書いてもよいし、job()と書くことも可能
 
 disconnect()
